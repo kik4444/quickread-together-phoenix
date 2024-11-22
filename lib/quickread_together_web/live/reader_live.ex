3cad9206289342543b3f77a47c662cb743e5497b
kik4444 <7779637+kik4444@users.noreply.github.com>
2024-11-22 10:16:21 +0200

    Move html to separate file

--- lib/quickread_together_web/live/reader_live.ex
+++ lib/quickread_together_web/live/reader_live.ex
@@ -3,27 +3,6 @@ defmodule QuickreadTogetherWeb.ReaderLive do
   alias Phoenix.PubSub
   alias QuickreadTogether.State
 
-  def render(assigns) do
-    ~H"""
-    <main class="h-screen w-screen grid place-items-center">
-      <.form for={@raw_text} phx-change="text_changed">
-        <textarea
-          id="textarea"
-          placeholder="Enter text to read quickly."
-          class="min-w-[50vw]"
-          name="raw_text"
-          phx-debounce="500"
-        ><%= @raw_text["value"] %></textarea>
-      </.form>
-
-      <p id="display" class="font-semibold leading-[1.3] tracking-normal antialiased">
-        TEMP
-      </p>
-      <button>Start</button>
-    </main>
-    """
-  end
-
   def mount(_params, _session, socket) do
     if connected?(socket), do: PubSub.subscribe(QuickreadTogether.PubSub, "reader:main")
 
