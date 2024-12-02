b51211f0aa2efbde16e86feced044b2b1ecf4b41
kik4444 <7779637+kik4444@users.noreply.github.com>
2024-12-02 20:07:04 +0200

    Increase line_length

--- lib/quickread_together_web/live/reader_live.ex
+++ lib/quickread_together_web/live/reader_live.ex
@@ -28,8 +28,7 @@ defmodule QuickreadTogetherWeb.ReaderLive do
      assign(socket,
        textarea: %{"raw_text" => state.raw_text},
        playing: state.playing,
-       current_chunk:
-         elem(state.parsed_text, clamp(state.current_index, 0, tuple_size(state.parsed_text) - 1)).chunk,
+       current_chunk: elem(state.parsed_text, clamp(state.current_index, 0, tuple_size(state.parsed_text) - 1)).chunk,
        textarea_locked: state.textarea_locked,
        controls: %{"words_per_minute" => state.words_per_minute, "chunk_size" => state.chunk_size}
      )}
